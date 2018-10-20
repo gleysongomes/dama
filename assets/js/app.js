6bdae4a3175e4a7707a28762124e4bea78c08a98
Gleyson Gomes <fgleysondasilva@gmail.com>
2018-10-19 21:07:13 -0300

    Mudança na forma como à aplicação é inicializada.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,13 @@
 define(function(require) {
+	var JogadorController = require("controller/JogadorController");
 	var TabuleiroController = require("controller/TabuleiroController");
-	return TabuleiroController;
+	var App = function(){
+	}
+	App.build = function(){
+		var tabuleiroController = new TabuleiroController();
+		tabuleiroController.criarTabuleiro();
+		var jogadorController = new JogadorController(tabuleiro);
+		jogadorController.initialize();
+	}
+	return App;
 });
